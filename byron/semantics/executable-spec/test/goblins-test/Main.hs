f28570b189d61c966ea5ae4c04e82fab4d4f8aec
Michael Hueschen <michaelhueschen@gmail.com>
2019-06-07 11:31:50 -0400

    Prepare for revamp of BreedingPit.breedStsGoblins API

--- byron/semantics/executable-spec/test/goblins-test/Main.hs
+++ byron/semantics/executable-spec/test/goblins-test/Main.hs
@@ -1,25 +1,24 @@
 {-# LANGUAGE TypeApplications #-}
-import Control.State.Transition.Goblin.BreedingPit (breedStsGoblins)
-import Test.Goblin.Explainer
-import Test.Goblin
-import Data.TreeDiff.Pretty
-import           Generator
-import LedgerState
-import Slot
+
+import           Data.TreeDiff.Pretty
 import qualified Data.TypeRepMap as TM
-import Control.Lens ((^.), _1, _2, _3, view)
-import Text.PrettyPrint (render)
+import           Text.PrettyPrint (render)
+
+import           Control.State.Transition.Goblin.BreedingPit (breedStsGoblins)
+import           Test.Goblin
+import           Test.Goblin.Explainer
 
 main :: IO ()
 main = do
-    pop <- breedStsGoblins jcGen (UTXOFailure [StakeKeyAlreadyRegistered])
-    case (filter ((> 100.0) . view _2) pop) of
-      (best:_) -> do
-        putStrLn $ "Best goblin scored " ++ show (best ^. _2)
-        let (Just diff) = explainGoblinGen (fmap (view _3) jcGen) (spawnGoblin (best ^. _1) TM.empty)
-        putStrLn $ render $ prettyEditExprCompact diff
-      [] -> putStrLn "No good goblins bred!"
-  where
-    jcGen = do
-      (_, steps, _, sig, ls) <- genValidStateTx
-      return (Slot steps, ls, sig)
+  pop <- breedStsGoblins sigGen (UTXOFailure [StakeKeyAlreadyRegistered])
+  case (filter ((> 100.0) . view _2) pop) of
+    (best:_) -> do
+      putStrLn $ "Best goblin scored " ++ show (best ^. _2)
+      let (Just diff) = explainGoblinGen (fmap (view _3) jcGen) (spawnGoblin (best ^. _1) TM.empty)
+      putStrLn $ render $ prettyEditExprCompact diff
+    [] -> putStrLn "No good goblins bred!"
+ where
+  startState = unwrapRule (head initialRules)
+  jcGen = do
+    (_, steps, _, sig, ls) <- genValidStateTx
+    return (Slot steps, ls, sig)

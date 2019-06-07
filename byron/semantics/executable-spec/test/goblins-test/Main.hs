aa6fa13b63eec9f46d777cf2fbab6cd393849fb3
Michael Hueschen <michaelhueschen@gmail.com>
2019-06-07 16:06:53 -0400

    Add goblins instances, make `Main` compile
    It only compiles with `stack repl`. Oddly, `stack build` fails with
    weird type errors. I don't understand.

--- byron/semantics/executable-spec/test/goblins-test/Main.hs
+++ byron/semantics/executable-spec/test/goblins-test/Main.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE TypeApplications #-}
 
-import           Data.TreeDiff.Pretty
-import qualified Data.TypeRepMap as TM
-import           Text.PrettyPrint (render)
+-- import           Data.TreeDiff.Pretty
+-- import qualified Data.TypeRepMap as TM
+-- import           Text.PrettyPrint (render)
 
-import           Cardano.Ledger.Spec.STS.UTXOW (UTXOW(..), PredicateFailure(..))
-import           Control.State.Transition (STS(..))
+import qualified Cardano.Ledger.Spec.STS.UTXOW as U (UTXOW, PredicateFailure(..))
+-- import           Control.State.Transition.Generator (HasTrace(..))
 import           Control.State.Transition.Goblin.BreedingPit (breedStsGoblins)
-import           Test.Goblin
-import           Test.Goblin.Explainer
+import           Instances ()
+-- import           Test.Goblin
+-- import           Test.Goblin.Explainer
 
 main :: IO ()
 main = do
-  pop <- breedStsGoblins @UTXOW InsufficientWitnesses
+  pop <- breedStsGoblins @U.UTXOW U.InsufficientWitnesses
   case (filter ((> 100.0) . snd) pop) of
     (best:_) -> do
       putStrLn $ "Best goblin scored " ++ show (snd best)

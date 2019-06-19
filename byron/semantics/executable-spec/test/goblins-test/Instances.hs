9117ec3c7d028a2f9f6d1528a35cac7ea671c42b
Michael Hueschen <michaelhueschen@gmail.com>
2019-06-19 19:44:11 -0400

    Goblins: add missing Goblins instances
    Now, I can run `goblins-test`, but I see:
    
    λ> main
    Top 5:
    *** Exception: divide by zero

--- byron/semantics/executable-spec/test/goblins-test/Instances.hs
+++ byron/semantics/executable-spec/test/goblins-test/Instances.hs
@@ -35,35 +35,100 @@ import           Test.Goblin
 --------------------------------------------------------------------------------
 
 instance Goblin Bool Addr where
+  tinker gen = do
+    gen' <- tinker ((\(Addr w) -> w) <$> gen)
+    pure (Addr <$> gen')
+  conjure = Addr <$$> conjure
 instance Goblin Bool Epoch where
-  tinker (Epoch w) = Epoch <$> tinker w
-  conjure = Epoch <$> conjure
+  tinker gen = do
+    gen' <- tinker ((\(Epoch w) -> w) <$> gen)
+    pure (Epoch <$> gen')
+  conjure = Epoch <$$> conjure
 instance Goblin Bool Hash where
-  tinker (Hash i) = Hash <$> tinker i
-  conjure = Hash <$> conjure
+  tinker gen = do
+    gen' <- tinker ((\(Hash w) -> w) <$> gen)
+    pure (Hash <$> gen')
+  conjure = Hash <$$> conjure
 instance Goblin Bool Lovelace where
-  tinker (Lovelace i) = Lovelace <$> tinker i
-  conjure = Lovelace <$> conjure
+  tinker gen = do
+    gen' <- tinker ((\(Lovelace w) -> w) <$> gen)
+    pure (Lovelace <$> gen')
+  conjure = Lovelace <$$> conjure
 instance Goblin Bool Owner where
-  tinker (Owner n) = Owner <$> tinker n
-  conjure = Owner <$> conjure
+  tinker gen = do
+    gen' <- tinker ((\(Owner w) -> w) <$> gen)
+    pure (Owner <$> gen')
+  conjure = Owner <$$> conjure
 instance Goblin Bool (Sig Tx) where
+  tinker gen = do
+    genX <- tinker ((\(Sig x _) -> x) <$> gen)
+    genY <- tinker ((\(Sig _ y) -> y) <$> gen)
+    pure (Sig <$> genX <*> genY)
+  conjure = (\x y -> Sig <$> x <*> y) <$> conjure <*> conjure
 instance Goblin Bool (Sig VKeyGenesis) where
+  tinker gen = do
+    genX <- tinker ((\(Sig x _) -> x) <$> gen)
+    genY <- tinker ((\(Sig _ y) -> y) <$> gen)
+    pure (Sig <$> genX <*> genY)
+  conjure = (\x y -> Sig <$> x <*> y) <$> conjure <*> conjure
 instance Goblin Bool VKey where
-  tinker (VKey o) = VKey <$> tinker o
-  conjure = VKey <$> conjure
+  tinker gen = do
+    gen' <- tinker ((\(VKey w) -> w) <$> gen)
+    pure (VKey <$> gen')
+  conjure = VKey <$$> conjure
 instance Goblin Bool VKeyGenesis where
-  tinker (VKeyGenesis k) = VKeyGenesis <$> tinker k
-  conjure = VKeyGenesis <$> conjure
+  tinker gen = do
+    gen' <- tinker ((\(VKeyGenesis w) -> w) <$> gen)
+    pure (VKeyGenesis <$> gen')
+  conjure = VKeyGenesis <$$> conjure
 
 
 instance Goblin Bool Tx where
+  tinker gen = do
+    genX <- tinker ((\(Tx x _) -> x) <$> gen)
+    genY <- tinker ((\(Tx _ y) -> y) <$> gen)
+    pure (Tx <$> genX <*> genY)
+  conjure = (\x y -> Tx <$> x <*> y) <$> conjure <*> conjure
 instance Goblin Bool TxId where
-  tinker (TxId h) = TxId <$> tinker h
-  conjure = TxId <$> conjure
+  tinker gen = do
+    gen' <- tinker ((\(TxId w) -> w) <$> gen)
+    pure (TxId <$> gen')
+  conjure = TxId <$$> conjure
 instance Goblin Bool TxIn where
+  tinker gen = do
+    genX <- tinker ((\(TxIn x _) -> x) <$> gen)
+    genY <- tinker ((\(TxIn _ y) -> y) <$> gen)
+    pure (TxIn <$> genX <*> genY)
+  conjure = (\x y -> TxIn <$> x <*> y) <$> conjure <*> conjure
 instance Goblin Bool TxOut where
+  tinker gen = do
+    genX <- tinker ((\(TxOut x _) -> x) <$> gen)
+    genY <- tinker ((\(TxOut _ y) -> y) <$> gen)
+    pure (TxOut <$> genX <*> genY)
+  conjure = (\x y -> TxOut <$> x <*> y) <$> conjure <*> conjure
 instance Goblin Bool TxWits where
+  tinker gen = do
+    genX <- tinker ((\(TxWits x _) -> x) <$> gen)
+    genY <- tinker ((\(TxWits _ y) -> y) <$> gen)
+    pure (TxWits <$> genX <*> genY)
+  conjure = (\x y -> TxWits <$> x <*> y) <$> conjure <*> conjure
 instance Goblin Bool Wit where
+  tinker gen = do
+    genX <- tinker ((\(Wit x _) -> x) <$> gen)
+    genY <- tinker ((\(Wit _ y) -> y) <$> gen)
+    pure (Wit <$> genX <*> genY)
+  conjure = (\x y -> Wit <$> x <*> y) <$> conjure <*> conjure
 
 instance Goblin Bool DCert where
+  tinker gen = do
+    genX <- tinker ((\(DCert x _ _ _) -> x) <$> gen)
+    genY <- tinker ((\(DCert _ y _ _) -> y) <$> gen)
+    genZ <- tinker ((\(DCert _ _ z _) -> z) <$> gen)
+    genW <- tinker ((\(DCert _ _ _ w) -> w) <$> gen)
+    pure (DCert <$> genX <*> genY <*> genZ <*> genW)
+  conjure = (\x y z w -> DCert <$> x <*> y <*> z *> w)
+        <$> conjure <*> conjure <*> conjure <*> conjure
+
+
+(<$$>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
+(<$$>) = fmap . fmap

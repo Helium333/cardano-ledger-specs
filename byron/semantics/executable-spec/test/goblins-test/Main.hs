36d5323b1dd2d75eecd18c336a4c841c85a57610
Michael Hueschen <michaelhueschen@gmail.com>
2019-06-20 14:34:56 -0400

    Goblins: run the explainer on best Goblin
    For the simple case of InsufficientWitnesses, it works, by deleting
    a witness.

--- byron/semantics/executable-spec/test/goblins-test/Main.hs
+++ byron/semantics/executable-spec/test/goblins-test/Main.hs
@@ -1,16 +1,39 @@
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE FlexibleInstances #-}
 
--- import           Data.TreeDiff.Pretty
--- import qualified Data.TypeRepMap as TM
--- import           Text.PrettyPrint (render)
+{-# OPTIONS_GHC -fno-warn-orphans  #-}
+import           Control.Monad (forM_)
+import           Data.TreeDiff.Class
+import           Data.TreeDiff.Expr
+import           Data.TreeDiff.Pretty
+import qualified Data.TypeRepMap as TM
+import           Text.PrettyPrint (render)
 
 import qualified Cardano.Ledger.Spec.STS.UTXOW as U (UTXOW, PredicateFailure(..))
 -- import           Control.State.Transition.Generator (HasTrace(..))
-import           Control.State.Transition.Goblin.BreedingPit (breedStsGoblins)
+import           Control.State.Transition.Goblin.BreedingPit (breedStsGoblins, genBlarg)
 import           Instances ()
 import           Ledger.Delegation (DELEG, PredicateFailure(..))
--- import           Test.Goblin
--- import           Test.Goblin.Explainer
+
+import           Ledger.Core
+  ( Addr(..)
+  , Hash(..)
+  , Lovelace(..)
+  , Owner(..)
+  , Sig(..)
+  , VKey(..)
+  )
+import           Ledger.UTxO
+  ( Tx(..)
+  , TxId(..)
+  , TxIn(..)
+  , TxOut(..)
+  , TxWits(..)
+  , Wit(..)
+  )
+
+import           Test.Goblin
+import           Test.Goblin.Explainer
 
 bar = breedStsGoblins @DELEG (SDelegSFailure (SDelegFailure IsAlreadyScheduled))
 
@@ -19,6 +42,31 @@ main = do
   pop <- breedStsGoblins @U.UTXOW U.InsufficientWitnesses
   putStrLn $ "Top 5: "
   mapM_ print (take 5 pop)
+  let best = head pop
+  let diffs = map (\g -> explainGoblinGen g (spawnGoblin (fst best) TM.empty))
+                  genSigs
+  forM_ diffs $ \(Just diff) ->
+    putStrLn $ render $ prettyEditExprCompact diff
+
+ where
+  genSigs = (snd <$>) <$> (genBlarg @U.UTXOW)
+
+instance ToExpr Addr where
+instance ToExpr Lovelace where
+  toExpr (Lovelace x) = App "Lovelace" [toExpr x]
+instance ToExpr Owner where
+  toExpr (Owner x) = App "Owner" [toExpr x]
+instance ToExpr (Sig Tx)
+instance ToExpr Tx
+instance ToExpr TxId where
+  toExpr (TxId (Hash i)) = App "TxId" [App "Hash" [toExpr i]]
+instance ToExpr TxIn
+instance ToExpr TxOut
+instance ToExpr TxWits
+instance ToExpr VKey where
+  toExpr (VKey x) = App "VKey" [toExpr x]
+instance ToExpr Wit
+
   -- case (filter ((> 100.0) . snd) pop) of
   --   (best:_) -> do
   --     putStrLn $ "Best goblin scored " ++ show (snd best)
@@ -26,6 +74,3 @@ main = do
   --     -- putStrLn $ render $ prettyEditExprCompact diff
   --   [] -> do
   --     putStrLn "No good goblins bred!"
-
-thd :: (a,b,c) -> c
-thd (_,_,x) = x
